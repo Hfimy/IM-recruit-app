9f8de567e8ea5b424443adc64434f8c9cf9c4e69
黄涛 <huangtao@sansi.com>
2018-04-07 16:56:22 +0800

    添加模块热替换

--- note.md
+++ note.md
@@ -1,4 +1,18 @@
 ### 开发笔记
 
-1.  初始化  
-    `creat-react-app bosszhipin --scripts-version=react-scripts-ts`
+1、 初始化
+
+```
+creat-react-app bosszhipin --scripts-version=react-scripts-ts
+```
+
+2、 弹射 create-react-app 配置
+
+```
+npm run eject
+```
+
+3、 添加开发模式模块热替换：
+react-hot-loader
+
+4、 在 tsconfig.json 里配置模块别名
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import * as React from 'react';
-import './App.css';
-
-const logo = require('./logo.svg');
+import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="App">hello ,typescript</div>;
   }
 }
 
-export default App;
+export default hot(module)(App);
